fc132b7efddd8cb14ac12dbe10c877d14a7a6c47
Tushar Anekar <anekar.tushar@gmail.com>
2023-06-23 23:06:24 +0530

    fix- warning message of table

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -55,34 +55,43 @@ export function Home() {
       <input type="text" onChange={handleSearch} />
       <button onClick={handleSearchBtn}>Search</button>
       <table>
-        <tr>
-          <th onClick={handleIDSort}>ID</th>
-          <th onClick={handleNameSort}>Product Name</th>
-          <th onClick={handleWeightSort}>Product Weight</th>
-          <th onClick={handlePriceSort}>Price</th>
-          <th onClick={handleCaloriesSort}>Calories</th>
-          <th>Ingredients</th>
-        </tr>
+        <tbody>
+          <tr>
+            <th onClick={handleIDSort}>ID</th>
+            <th onClick={handleNameSort}>Product Name</th>
+            <th onClick={handleWeightSort}>Product Weight</th>
+            <th onClick={handlePriceSort}>Price</th>
+            <th onClick={handleCaloriesSort}>Calories</th>
+            <th>Ingredients</th>
+          </tr>
 
-        {searchedSnacks.map(
-          ({
-            id,
-            product_name,
-            product_weight,
-            price,
-            calories,
-            ingredients,
-          }) => (
-            <tr key={id}>
-              <td>{id}</td>
-              <td>{product_name}</td>
-              <td>{product_weight}</td>
-              <td>{price}</td>
-              <td>{calories}</td>
-              <td>{ingredients.map((ingredient, index) => <span>{ingredient}{index < ingredients.length - 1 ? "," : ""}</span>)}</td>
-            </tr>
-          )
-        )}
+          {searchedSnacks.map(
+            ({
+              id,
+              product_name,
+              product_weight,
+              price,
+              calories,
+              ingredients,
+            }) => (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{product_name}</td>
+                <td>{product_weight}</td>
+                <td>{price}</td>
+                <td>{calories}</td>
+                <td>
+                  {ingredients.map((ingredient, index) => (
+                    <span key={`${id}${index}`}>
+                      {ingredient}
+                      {index < ingredients.length - 1 ? "," : ""}
+                    </span>
+                  ))}
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
       </table>
     </>
   );
