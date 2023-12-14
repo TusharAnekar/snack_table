56e2c6fb0456fb732ca09e2b264118d1b57f2fae
Tushar Anekar <anekar.tushar@gmail.com>
2023-12-14 14:41:40 +0530

    refactor- removed redudant code

--- src/contexts/snacks-context.jsx
+++ src/contexts/snacks-context.jsx
@@ -35,56 +35,38 @@ const SnacksProvider = ({ children }) => {
   const getSortedSnacks = () => {
     switch (tableHeaderName) {
       case "ID":
-        return (
-          idSortType !== "" &&
-          [...searchFilteredSnacks].sort((a, b) =>
-            idSortType === "LTH" ? a.id - b.id : b.id - a.id
-          )
+        return [...searchFilteredSnacks].sort((a, b) =>
+          idSortType === "LTH" ? a.id - b.id : b.id - a.id
         );
       case "NAME":
-        return (
-          productNameSortType !== "" &&
-          [...searchFilteredSnacks].sort((a, b) =>
-            productNameSortType === "LTH"
-              ? a.product_name.localeCompare(b.product_name)
-              : b.product_name.localeCompare(a.product_name)
-          )
+        return [...searchFilteredSnacks].sort((a, b) =>
+          productNameSortType === "LTH"
+            ? a.product_name.localeCompare(b.product_name)
+            : b.product_name.localeCompare(a.product_name)
         );
       case "WEIGHT":
-        return (
-          productWeightSortType !== "" &&
-          [...searchFilteredSnacks].sort((a, b) =>
-            productWeightSortType === "LTH"
-              ? Number(a.product_weight.slice(0, -1)) -
-                Number(b.product_weight.slice(0, -1))
-              : Number(b.product_weight.slice(0, -1)) -
-                Number(a.product_weight.slice(0, -1))
-          )
+        return [...searchFilteredSnacks].sort((a, b) =>
+          productWeightSortType === "LTH"
+            ? Number(a.product_weight.slice(0, -1)) -
+              Number(b.product_weight.slice(0, -1))
+            : Number(b.product_weight.slice(0, -1)) -
+              Number(a.product_weight.slice(0, -1))
         );
       case "PRICE":
-        return (
-          priceSortType !== "" &&
-          [...searchFilteredSnacks].sort((a, b) =>
-            priceSortType === "LTH" ? a.price - b.price : b.price - a.price
-          )
+        return [...searchFilteredSnacks].sort((a, b) =>
+          priceSortType === "LTH" ? a.price - b.price : b.price - a.price
         );
       case "CALORIES":
-        return (
-          caloriesSortType !== "" &&
-          [...searchFilteredSnacks].sort((a, b) =>
-            caloriesSortType === "LTH"
-              ? a.calories - b.calories
-              : b.calories - a.calories
-          )
+        return [...searchFilteredSnacks].sort((a, b) =>
+          caloriesSortType === "LTH"
+            ? a.calories - b.calories
+            : b.calories - a.calories
         );
       case "INGREDIENTS":
-        return (
-          ingredientsSortType !== "" &&
-          [...searchFilteredSnacks].sort((a, b) =>
-            ingredientsSortType === "LTH"
-              ? a.ingredients.join("").localeCompare(b.ingredients.join(""))
-              : b.ingredients.join("").localeCompare(a.ingredients.join(""))
-          )
+        return [...searchFilteredSnacks].sort((a, b) =>
+          ingredientsSortType === "LTH"
+            ? a.ingredients.join("").localeCompare(b.ingredients.join(""))
+            : b.ingredients.join("").localeCompare(a.ingredients.join(""))
         );
       default:
         return searchFilteredSnacks;
